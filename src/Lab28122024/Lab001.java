05f588d92675d036a9789f4f5389ac34f6019fc7
navaneethhk20 <hknavaneeth@gmail.com>
2024-12-29 07:41:28 +0530

    commit

---
+++ src/Lab28122024/Lab001.java
@@ -0,0 +1,18 @@
+package Lab28122024;
+
+public class Lab001 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lab");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        int age = 65;
+        System.out.println(age);
+        age = -6;
+        System.out.println(age);
+
+    }
+}
---
+++ src/Lab29122024/Lab001.java
@@ -0,0 +1,9 @@
+package Lab29122024;
+
+public class Lab001
+{
+    public static void main(String[] args) {
+
+    }
+}
+
