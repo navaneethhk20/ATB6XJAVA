d76c0afae304b7dd6a1701223d09c0874cbfe51d
navaneethhk20 <hknavaneeth@gmail.com>
2024-12-31 08:27:51 +0530

    commit

---
+++ src/Lab31122024/Lab001.java
@@ -0,0 +1,16 @@
+package Lab31122024;
+
+import java.sql.SQLOutput;
+
+public class Lab001 {
+    public static void main (String[] args)
+    {
+        int a= 10;
+        System.out.println(++a + a++ + a++);
+
+        int b=10;
+        System.out.println(--b + b++ + ++b);
+
+    }
+
+}
