b3236f92a1905692ccc2496936ba1a661bd7a275
navaneethhk20 <hknavaneeth@gmail.com>
2025-01-04 07:52:24 +0530

    commit 04

---
+++ src/Lab04012025/Lab001.java
@@ -0,0 +1,10 @@
+package Lab04012025;
+
+import java.sql.SQLOutput;
+
+public class Lab001 {
+    public static void main (String[] args){
+        for(int i=0 ; i<= 10 ; i++)
+            System.out.println("Enter name");
+    }
+}
