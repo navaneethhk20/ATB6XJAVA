b79857b3f4e569feeaed0dade7ab6ab15adfa522
navaneethhhk <hknavaneeth@gmail.com>
2025-06-04 19:57:06 +0530

    commit 0406

---
+++ src/Lab04062025/Lab01.java
@@ -0,0 +1,7 @@
+package Lab04062025;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
--- src/Lab29122024/Lab001.java
+++ src/Lab29122024/Lab001.java
@@ -5,7 +5,6 @@ public class Lab001
     public static void main(String[] args) {
     System.out.println("t");
     System.out.println("t1");
-
     }
 }
 
