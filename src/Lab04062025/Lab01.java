2346002cc318685579ddc48a6e74ffffdd149f7e
navaneethhhk <hknavaneeth@gmail.com>
2025-06-05 07:43:10 +0530

    commit 0506

--- src/Lab04062025/Lab01.java
+++ src/Lab04062025/Lab01.java
@@ -3,5 +3,21 @@ package Lab04062025;
 public class Lab01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("10 x 1 = 10");
+        System.out.println("10 x 2 = 20");
+        System.out.println("10 x 3 = 30");
+        System.out.println("10 x 4 = 40");
+        System.out.println("10 x 5 = 50");
+        System.out.println("10 x 6 = 60");
+        System.out.println("10 x 7 = 70");
+        System.out.println("10 x 8 = 80");
+        System.out.println("10 x 9 = 90");
+        System.out.println("10 x 10 = 100");
+        System.out.println(" -------------------- ");
+        System.out.println(10+10);
+        System.out.println(10-10);
+        System.out.println(10*10);
+        System.out.println(10/10);
+        System.out.println(10%2);
     }
 }
