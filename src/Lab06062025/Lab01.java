b17f61f029eeed206795ba4dbe393c6555e1f431
navaneethhhk <hknavaneeth@gmail.com>
2025-06-06 07:05:59 +0530

    commit 0606

--- src/Lab06062025/Lab01.java
+++ src/Lab06062025/Lab01.java
@@ -2,8 +2,8 @@ package Lab06062025;
 
 public class Lab01 {
     public static void main(String[] args) {
-         int a =10;
-        a=11;
+        final int a =10;
+       // a=11;
         System.out.println(a);
     }
 }
