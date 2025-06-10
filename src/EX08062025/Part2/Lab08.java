c2727bcb3a442d2d8069320d77edbc79bfbeca10
navaneethhhk <hknavaneeth@gmail.com>
2025-06-10 07:32:24 +0530

    commit 1006

--- src/EX08062025/Part2/Lab08.java
+++ src/EX08062025/Part2/Lab08.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Lab08 {
     public static void main(String[] args) {
         //prime number
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing prime numbers");
+
 
     }
 }
