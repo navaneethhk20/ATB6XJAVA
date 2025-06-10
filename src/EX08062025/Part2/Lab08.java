45831eced609792532d62188291452550d1f11d0
navaneethhhk <hknavaneeth@gmail.com>
2025-06-10 07:28:24 +0530

    commit 1006

--- src/EX08062025/Part2/Lab08.java
+++ src/EX08062025/Part2/Lab08.java
@@ -1,8 +1,11 @@
 package EX08062025.Part2;
 
+import java.util.Scanner;
+
 public class Lab08 {
     public static void main(String[] args) {
         //prime number
+        Scanner sc = new Scanner(System.in);
 
     }
 }
