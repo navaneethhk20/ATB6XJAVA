4c3ccb4ca1797ae43119c612d2112e14cd1db5a3
navaneethhhk <hknavaneeth@gmail.com>
2025-06-11 07:16:51 +0530

    commit 1106

--- src/Ex11062025/L2.java
+++ src/Ex11062025/L2.java
@@ -1,8 +1,19 @@
 package Ex11062025;
 
+import java.util.Scanner;
+
 public class L2 {
     public static void main(String[] args) {
         //Right angled traingle
-
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        for(int i=0;i<num;i++){
+            for(int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
