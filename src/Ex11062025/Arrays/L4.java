fcd66a919028f798c4bf49421c899f9f0ee957ce
navaneethhhk <hknavaneeth@gmail.com>
2025-06-17 06:56:26 +0530

    commit 1106

--- src/Ex11062025/Arrays/L4.java
+++ src/Ex11062025/Arrays/L4.java
@@ -2,6 +2,7 @@ package Ex11062025.Arrays;
 
 public class L4 {
     public static void main(String[] args) {
+        //Right traingle program
         int n=6;
         for(int i=0;i<n;i++){
             for (int j=0;j<i;j++){
