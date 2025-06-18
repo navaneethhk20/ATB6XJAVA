472fd1f7263c6baa0482ecce2e5c516c11442753
navaneethhhk <hknavaneeth@gmail.com>
2025-06-18 07:09:26 +0530

    commit 1106

---
+++ src/Ex18062025/Part1/Lab1.java
@@ -0,0 +1,17 @@
+package Ex18062025.Part1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        //triangle
+        int row =5;
+        for(int i=1;i<=row;i++) {
+            for (int j = 1; j <=row-i; j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
