30c40f4c85231e23c382b65822d7bada47548a9e
navaneethhhk <hknavaneeth@gmail.com>
2025-06-22 10:43:21 +0530

    commit 2006

---
+++ src/Ex22062025/Lab02.java
@@ -0,0 +1,8 @@
+package Ex22062025;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        //Find out middle unique characters in string  "switchas switches que testing"
+
+    }
+}
