9a908a67ddb3520220d7c467340f1524f5b81f93
navaneethhhk <hknavaneeth@gmail.com>
2025-06-22 10:42:09 +0530

    commit 2006

--- src/Ex22062025/Lab01.java
+++ src/Ex22062025/Lab01.java
@@ -2,7 +2,8 @@ package Ex22062025;
 
 public class Lab01 {
     public static void main(String[] args){
-
+//           Input "I Love Java Programing"
+//            Output "I Love Java gnimargorP"
     }
 
 }
