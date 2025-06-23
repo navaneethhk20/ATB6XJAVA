c774f4fe3f2437bb8cbe53441610234987b9b680
navaneethhhk <hknavaneeth@gmail.com>
2025-06-23 07:08:34 +0530

    commit 2006

--- src/Ex22062025/Lab01.java
+++ src/Ex22062025/Lab01.java
@@ -4,6 +4,24 @@ public class Lab01 {
     public static void main(String[] args){
 //           Input "I Love Java Programing"
 //            Output "I Love Java gnimargorP"
-    }
+
+                String input = "I Love Java Programming";
+                String output = reverseLastWord(input);
+                System.out.println(input);
+                System.out.println(output);
+            }
+
+            public static String reverseLastWord(String sentence) {
+                // Split the sentence into words
+                String[] words = sentence.split(" ");
+
+                // Reverse the last word
+                String lastWord = words[words.length - 1];
+                StringBuilder reversed = new StringBuilder(lastWord);
+                words[words.length - 1] = reversed.reverse().toString();
+
+                // Join the words back together
+                return String.join(" ", words);
+            }
 
 }
