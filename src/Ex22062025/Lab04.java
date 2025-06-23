bfc807249f56a6b237264c1ccd95f3b99a6c87b8
navaneethhhk <hknavaneeth@gmail.com>
2025-06-23 07:10:25 +0530

    commit 2006

---
+++ src/Ex22062025/Lab04.java
@@ -0,0 +1,8 @@
+package Ex22062025;
+
+public class Lab04 {
+    public static void main(String[] args) {
+        //Input = "abcd123@#$%"
+      //  output: split into character, numbers and special charracters
+    }
+}
